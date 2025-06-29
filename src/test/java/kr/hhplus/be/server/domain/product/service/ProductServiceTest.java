4bb6682607484545edcaf28955df0174c6b761a4
Dior <chinup1004@naver.com>
2025-06-30 00:36:26 +0900

    [TEST] 주문 조회 단위 테스트 Update

--- src/test/java/kr/hhplus/be/server/domain/product/service/ProductServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/product/service/ProductServiceTest.java
@@ -1,6 +1,9 @@
 package kr.hhplus.be.server.domain.product.service;
 
+import domain.product.cache.ProductCache;
 import domain.product.entity.Product;
+import domain.product.repository.ProductRepository;
+import domain.product.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +17,7 @@ public class ProductServiceTest {
 
     private ProductService productService;
     private ProductRepository productRepository;
-    private ProductCacheRepository productCacheRepository;
+    private ProductCache productCache;
 
     private final List<Product> mockProductList = List.of(
             Product.builder().id(1L).name("상품A").price(1000L).stock(10).build(),
@@ -24,21 +27,21 @@ public class ProductServiceTest {
     @BeforeEach
     void setUp() {
         productRepository = mock(ProductRepository.class);
-        productCacheRepository = mock(ProductCacheRepository.class);
-        productService = new ProductService(productRepository, productCacheRepository);
+        productCache = mock(ProductCache.class);
+        productService = new ProductService(productRepository, productCache);
     }
 
     @Test
     @DisplayName("캐시에 상품 목록이 존재하면 DB 조회 없이 캐시 반환")
     void getProducts_from_cache() {
         //given
-        when(productCacheRepository.getAllProducts()).thenReturn(mockProductList);
+        when(productCache.getAllProducts()).thenReturn(mockProductList);
 
         // when
         List<Product> result = productService.getProducts();
 
         // then
-        verify(productCacheRepository, times(1)).getAllProducts();
+        verify(productCache, times(1)).getAllProducts();
         verify(productRepository, never()).findAll();
         assertThat(result).isEqualTo(mockProductList);
     }
@@ -47,15 +50,15 @@ public class ProductServiceTest {
     @DisplayName("캐시에 없으면 DB 조회 후 캐시에 저장하고 반환")
     void getProducts_from_db_then_cache() {
         // given
-        when(productCacheRepository.getAllProducts()).thenReturn(null);
+        when(productCache.getAllProducts()).thenReturn(null);
         when(productRepository.findAll()).thenReturn(mockProductList);
 
         // when
         List<Product> result = productService.getProducts();
 
         // then
-        verify(productRepository, times(1)).findALl();
-        verify(productCacheRepository, times(1)).saveAllProducts(mockProductList);
+        verify(productRepository, times(1)).findAll();
+        verify(productCache, times(1)).saveAllProducts(mockProductList);
         assertThat(result).isEqualTo(mockProductList);
     }
 }
