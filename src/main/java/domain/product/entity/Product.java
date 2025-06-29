be688e0b15a129fd16123ca66646b8526aa479a7
Dior <chinup1004@naver.com>
2025-06-29 23:54:21 +0900

    [FEAT] Product 엔티티 작성

---
+++ src/main/java/domain/product/entity/Product.java
@@ -0,0 +1,28 @@
+package domain.product.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long price;
+
+    private Integer stock;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
